efac98f272356b00083101f3387b913272e0e611
dmoondev <dmoondev.17@gmail.com>
2024-07-20 06:26:07 -0300

    Corrección de advertencias en los Controllers

--- src/main/java/com/pespasioninterior/demo_ppi/Security/Controller/AuthController.java
+++ src/main/java/com/pespasioninterior/demo_ppi/Security/Controller/AuthController.java
@@ -8,7 +8,6 @@ import com.pespasioninterior.demo_ppi.Security.Service.AuthService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
